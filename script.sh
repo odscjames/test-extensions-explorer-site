95f6cf5b0c59acca547002caca2288354b67d98a
James Baster <james.baster@opendataservices.coop>
2018-09-14 13:21:59 +0100

    echo

--- script.sh
+++ script.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-# get data!
+echo "get data!"
 cd extensions-data-collector/repo
 source .ve/bin/activate
 python cli.py
 cd ../..
 
-# move data
+echo "move data"
 cp extensions-data-collector/repo/output_dir/data.json extension-explorer/repo/extension_explorer/data.json
 
-# freeze site
+echo "freeze site"
 cd extension-explorer/repo
 source .ve/bin/activate
 python freeze.py
 cd ../..
 
-# temp show output
+echo "temp show output"
 find extension-explorer/repo/extension_explorer/build
